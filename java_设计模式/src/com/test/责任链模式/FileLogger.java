311e96e9fa825d71970cbad4f6d65294edf000f0
wolf <qswdsh@sohu.com>
2018-02-06 20:16:27 +0800

    责任链模式

---
+++ java_设计模式/src/com/test/责任链模式/FileLogger.java
@@ -0,0 +1,13 @@
+package com.test.责任链模式;
+
+public class FileLogger extends AbstractLogger {
+
+	   public FileLogger(int level){
+	      this.level = level;
+	   }
+
+	   @Override
+	   protected void write(String message) {        
+	      System.out.println("File::Logger: " + message);
+	   }
+	}
