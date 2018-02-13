0c8150791b8ad387a06a6c4e949ed7ae3c6c1473
wolf <qswdsh@sohu.com>
2018-02-13 11:54:05 +0800

    备忘录模式

---
+++ java_设计模式/src/com/test/备忘录模式/Memento.java
@@ -0,0 +1,13 @@
+package com.test.±¸ÍüÂ¼Ä£Ê½;
+
+public class Memento {
+	   private String state;
+
+	   public Memento(String state){
+	      this.state = state;
+	   }
+
+	   public String getState(){
+	      return state;
+	   }    
+	}
