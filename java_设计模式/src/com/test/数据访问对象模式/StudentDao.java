34aa8d984f5dacda20cfa529d9205adcfdbc50af
wolf <qswdsh@sohu.com>
2018-03-15 19:11:19 +0800

    数据访问对象模式

---
+++ java_设计模式/src/com/test/数据访问对象模式/StudentDao.java
@@ -0,0 +1,10 @@
+package com.test.数据访问对象模式;
+
+import java.util.List;
+
+public interface StudentDao {
+	   public List<Student> getAllStudents();
+	   public Student getStudent(int rollNo);
+	   public void updateStudent(Student student);
+	   public void deleteStudent(Student student);
+	}
