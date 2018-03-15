e73125ea5d850f23e439f2525e1aa2f01b7173da
wolf <qswdsh@sohu.com>
2018-03-15 16:48:53 +0800

    组合实体模式

---
+++ java_设计模式/src/com/test/组合实体模式/CompositeEntity.java
@@ -0,0 +1,13 @@
+package com.test.组合实体模式;
+
+public class CompositeEntity {
+	   private CoarseGrainedObject cgo = new CoarseGrainedObject();
+
+	   public void setData(String data1, String data2){
+	      cgo.setData(data1, data2);
+	   }
+
+	   public String[] getData(){
+	      return cgo.getData();
+	   }
+	}
---
+++ java_设计模式/src/com/test/组合实体模式/DependentObject1.java
@@ -0,0 +1,14 @@
+package com.test.组合实体模式;
+
+public class DependentObject1 {
+    
+	   private String data;
+
+	   public void setData(String data){
+	      this.data = data; 
+	   } 
+
+	   public String getData(){
+	      return data;
+	   }
+	}
